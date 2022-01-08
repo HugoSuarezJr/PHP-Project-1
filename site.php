cbf15716ef8d654f1fc7abe9f03bae486bf970d5
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-07 20:41:31 -0500

    Input Forms

--- site.php
+++ site.php
@@ -7,8 +7,15 @@
     <title>PHP course</title>
 </head>
 <body>
-    <?php
-        echo max(2,10)
-    ?>
-</body> <h1></h1>
+    <form action="site.php" method= "get">
+        Name: <input type="text" name="name">
+        Age: <input type="number" name="age">
+        <input type="submit">
+    </form>
+    <br>
+    Your name is <?php echo $_GET["name"]; ?>
+    <br>
+    Your age is <?php echo $_GET["age"]; ?>
+    
+</body>
 </html>
