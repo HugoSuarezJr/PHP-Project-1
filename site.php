2079e10bb3553d8aa26a3266025f0e4fa1ae2440
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-07 20:49:59 -0500

    Create simple calculator

--- site.php
+++ site.php
@@ -8,14 +8,15 @@
 </head>
 <body>
     <form action="site.php" method= "get">
-        Name: <input type="text" name="name">
-        Age: <input type="number" name="age">
+        <input type="number" name="num1">
+        <br>
+        Plus (+)
+        <br>
+        <input type="number" name="num2">
         <input type="submit">
     </form>
-    <br>
-    Your name is <?php echo $_GET["name"]; ?>
-    <br>
-    Your age is <?php echo $_GET["age"]; ?>
+
+   Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
     
 </body>
 </html>
