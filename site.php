0ced40944f09bbc740642be84f3a73de256b0bd9
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-07 19:44:59 -0500

    first PHP file

---
+++ site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP course</title>
+</head>
+<body>
+    <?php
+        $characterName = "Tom";
+        $characterAge = 80;
+        echo "There once was a man named $characterName <br>";
+        echo "He was $characterAge years old <br>";
+        $characterName = "Mike";
+        echo "He really liked the name $characterName <br>";
+        echo "But didn't like being $characterAge <br>";   
+        echo "<h1>$characterName</h1>";
+        echo "<h1>$characterAge</h1>";
+    ?>
+</body> <h1></h1>
+</html>
