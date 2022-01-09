448dd6a0d0ed17eb6f94d4186fe1b58f1fc05682
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-09 01:49:02 -0500

    Use correct sorting order for alphanumeric arrays

--- site.php
+++ site.php
@@ -8,30 +8,44 @@
 </head>
 <body>
 
-<form action="site.php" method="post" >
+<!-- <form action="site.php" method="post" >
     First Number: <input type="number" step="0.0001" name="num1" > <br>
     OP: <input type="text" name="op"><br>
     Second Number: <input type="number" step="0.0001" name="num2" > <br>
     <input type="submit">
 </form>
+-->
 
-    <?php
-        $num1 = $_POST["num1"];
-        $num2 = $_POST["num2"];
-        $op = $_POST["op"];
+     <?php
+        // $num1 = $_POST["num1"];
+        // $num2 = $_POST["num2"];
+        // $op = $_POST["op"];
 
-        if($op == "+"){
-            echo $num1 + $num2;
-        } elseif($op == "*"){
-            echo $num1 * $num2;
-        } elseif($op == "/"){
-            echo $num1 / $num2;
-        } elseif($op == "-"){
-            echo $num1 - $num2;
-        } else {
-          echo  "Invalid Operator";
+        // if($op == "+"){
+        //     echo $num1 + $num2;
+        // } elseif($op == "*"){
+        //     echo $num1 * $num2;
+        // } elseif($op == "/"){
+        //     echo $num1 / $num2;
+        // } elseif($op == "-"){
+        //     echo $num1 - $num2;
+        // } else {
+        //   echo  "Invalid Operator";
+        // }
+
+        
+
+        $territories = array(
+            "1 - Miami", "10 - Key West", "20c - West Palm Beach", "20b - West Palm Beach", "2 - Fort Lauderdale", "5 - Orlando"
+        );
+        sort($territories, SORT_NATURAL | SORT_FLAG_CASE);
+        foreach ($territories as $key => $val) {
+            echo " $val \n <br>";
         }
-    ?>
+
+
+        
+    ?> 
     
     
 </body>
