af113d7bec9f6d5517b806a85f72aad1625aa5fb
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-09 01:04:33 -0500

    More advanced calculator

--- site.php
+++ site.php
@@ -8,23 +8,29 @@
 </head>
 <body>
 
-<!-- <form action="site.php" method="post" >
-    <input type="text" name="student" >
+<form action="site.php" method="post" >
+    First Number: <input type="number" step="0.0001" name="num1" > <br>
+    OP: <input type="text" name="op"><br>
+    Second Number: <input type="number" step="0.0001" name="num2" > <br>
     <input type="submit">
-</form> -->
+</form>
 
     <?php
-        function getMax($num1, $num2, $num3){
-            if($num1 >= $num2 && $num1 >= $num3){
-                return $num1;
-            } elseif($num2 >= $num1 && $num2 >= $num3) {
-                return $num2;
-            } else {
-                return $num3;
-            }
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $op = $_POST["op"];
+
+        if($op == "+"){
+            echo $num1 + $num2;
+        } elseif($op == "*"){
+            echo $num1 * $num2;
+        } elseif($op == "/"){
+            echo $num1 / $num2;
+        } elseif($op == "-"){
+            echo $num1 - $num2;
+        } else {
+          echo  "Invalid Operator";
         }
-        
-        echo getMax(300, 90, 342);
     ?>
     
     
