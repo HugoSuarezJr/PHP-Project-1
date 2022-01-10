a6576470e8e08d2403de15b39e66dc261abec2c2
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-10 18:34:14 -0500

    Test commit

--- site.php
+++ site.php
@@ -18,23 +18,16 @@
             var $author;
             var $pages;
         }
-
+ 
         $book1 = new Book;
-
         $book1-> title = "Harry Potter";
         $book1-> author = "JK Rowling";
         $book1-> pages = 400;
 
-        echo $book1-> pages;
-
         $book2 = new Book;
-
         $book2-> title = "Lord of The Rings";
         $book2-> author = "Tolkien";
         $book2-> pages = 700;
-
-        echo "<br>";
-        echo $book2-> pages;
 
      ?> 
     
