5b4e6cc4a3b2e5e979f642f9b997ffce11978452
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-01-10 16:00:46 -0500

    Do-While Loops

--- site.php
+++ site.php
@@ -8,49 +8,19 @@
 </head>
 <body>
 
-<form action="site.php" method="post" >
+<!-- <form action="site.php" method="post" >
     What was your grade? <br> <input type="text" name="grade" > <br>
-    <input type="submit">
+    <input type="submit"> -->
 </form>
 
 
      <?php
-       
-        $grade = $_POST["grade"];
+            $index = 6;
 
-        switch($grade){
-            case "A" : 
-                echo "You did amazing";
-                break;
-            case "B":
-                echo "You did pretty good!";
-                break;
-            case "C": 
-                echo "You did ok";
-                break;
-            case "D":
-                echo "You did very bad";
-                break;
-            case "F":
-                echo "YOU FAIL!";
-                break;
-            default:
-                echo "Invalid Grade";
-        }
-
-
-        
-
-        // $territories = array(
-        //     "1 - Miami", "10 - Key West", "20c - West Palm Beach", "20b - West Palm Beach", "2 - Fort Lauderdale", "5 - Orlando"
-        // );
-        // sort($territories, SORT_NATURAL | SORT_FLAG_CASE);
-        // foreach ($territories as $key => $val) {
-        //     echo " $val \n <br>";
-        // }
-
-
-        
+            do{
+                echo "$index <br>";
+                $index++;
+            }while($index <= 5);
     ?> 
     
     
